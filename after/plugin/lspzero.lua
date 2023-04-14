cad41468d94e20c2f30ea751076403a52b957430
Paul Mana <pmana@local.sequiter.com>
2023-04-14 08:05:32 -0600

    omnisharp work around

--- after/plugin/lspzero.lua
+++ after/plugin/lspzero.lua
@@ -1,6 +1,16 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
+     if client.name == 'omnisharp' then
+        local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+        for i, v in ipairs(tokenModifiers) do
+            tokenModifiers[i] = v:gsub(' ', '_')
+        end
+        local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+        for i, v in ipairs(tokenTypes) do
+        tokenTypes[i] = v:gsub(' ', '_')
+        end
+    end
   lsp.default_keymaps({buffer = bufnr})
 end)
 
