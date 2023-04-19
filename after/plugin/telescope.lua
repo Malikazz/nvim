2c8d0135ccf9dae178d8eab1224f813ab97de3d4
Paul Mana <pmana@local.sequiter.com>
2023-04-19 14:30:45 -0600

    live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,4 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ")});
-end)
+vim.keymap.set("n", "<leader>pg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
